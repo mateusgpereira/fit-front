ffad77526c731cdf8235fd2ea808e11e82573dc8
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2022-04-24 00:26:55 +0100

    removing karma and jasmine, adding jest, fixing tests

--- src/app/loading/loading.interceptor.spec.ts
+++ src/app/loading/loading.interceptor.spec.ts
@@ -1,16 +1,39 @@
+import { HttpRequest } from '@angular/common/http'
 import { TestBed } from '@angular/core/testing'
-
+import { Observable } from 'rxjs'
 import { LoadingInterceptor } from './loading.interceptor'
+import { LoadingService } from './loading.service'
 
 describe('LoadingInterceptor', () => {
-  beforeEach(() =>
+  let interceptor: LoadingInterceptor
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [LoadingInterceptor]
+      providers: [LoadingInterceptor, LoadingService]
     })
-  )
+    interceptor = TestBed.inject(LoadingInterceptor)
+  })
 
   it('should be created', () => {
-    const interceptor: LoadingInterceptor = TestBed.inject(LoadingInterceptor)
     expect(interceptor).toBeTruthy()
+  })
+
+  it('should correctly call show and hide of loadingService ', () => {
+    const loadingService = TestBed.inject(LoadingService)
+    jest.spyOn(loadingService, 'show')
+    jest.spyOn(loadingService, 'hide')
+
+    const next: any = {
+      handle: () => {
+        return new Observable((subscriber) => {
+          subscriber.complete()
+        })
+      }
+    }
+
+    interceptor.intercept(new HttpRequest('GET', '/test'), next).subscribe()
+
+    expect(loadingService.show).toBeCalled()
+    expect(loadingService.hide).toBeCalled()
   })
 })
