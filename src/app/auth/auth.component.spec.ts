4855e5c1e5d7f46d6e0a6ddb14ad18e7adaf70c8
Mateus Pereira <mateusgoncalvespereira@gmail.com>
2022-04-24 13:37:36 +0100

    fix auth.component tests

--- src/app/auth/auth.component.spec.ts
+++ src/app/auth/auth.component.spec.ts
@@ -1,15 +1,26 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing'
-
+import { FormsModule } from '@angular/forms'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { provideMockStore, MockStore } from '@ngrx/store/testing'
 import { AuthComponent } from './auth.component'
+import { AuthState } from './store/auth.state'
 
 describe('AuthComponent', () => {
   let component: AuthComponent
   let fixture: ComponentFixture<AuthComponent>
+  let store: MockStore
+  const initialState: AuthState = {
+    token: '',
+    error: ''
+  }
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [AuthComponent]
+      declarations: [AuthComponent],
+      providers: [provideMockStore({ initialState })],
+      imports: [FormsModule, FontAwesomeModule]
     }).compileComponents()
+    store = TestBed.inject(MockStore)
   })
 
   beforeEach(() => {
--- src/app/loading/loading.component.spec.ts
+++ src/app/loading/loading.component.spec.ts
@@ -1,25 +1,28 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
+import { NgxLoadingModule } from 'ngx-loading'
 
-import { LoadingComponent } from './loading.component';
+import { LoadingComponent } from './loading.component'
+import { LoadingService } from './loading.service'
 
 describe('LoadingComponent', () => {
-  let component: LoadingComponent;
-  let fixture: ComponentFixture<LoadingComponent>;
+  let component: LoadingComponent
+  let fixture: ComponentFixture<LoadingComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LoadingComponent ]
-    })
-    .compileComponents();
-  });
+      imports: [NgxLoadingModule],
+      declarations: [LoadingComponent],
+      providers: [LoadingService]
+    }).compileComponents()
+  })
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(LoadingComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(LoadingComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
